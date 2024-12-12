9e4120d26a7fa79ffe23abdcce12d911fc77f287
EugenyB <eberkunsky@gmail.com>
2024-12-12 20:04:02 +0200

    Some drawings

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@ int main()
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-    // Create a graphical text to display
-    sf::Font font;
-
-    sf::Text text("Hello SFML", font, 50);
 
     // Start the game loop
     while (window.isOpen())
@@ -31,8 +27,28 @@ int main()
         // Clear screen
         window.clear();
 
-        // Draw the string
-        window.draw(text);
+        sf::CircleShape shape(50.f);
+        shape.setFillColor(sf::Color(150, 50, 250));
+
+        // set a 10-pixel wide orange outline
+        shape.setOutlineThickness(10.f);
+        shape.setOutlineColor(sf::Color(250, 150, 100));
+
+        window.draw(shape);
+
+        sf::Vertex line[] =
+        {
+            sf::Vertex(sf::Vector2f(0.f, 0.f)),
+            sf::Vertex(sf::Vector2f(800.f, 600.f)),
+            sf::Vertex(sf::Vector2f(800.f, 0.f)),
+            sf::Vertex(sf::Vector2f(0.f, 600.f))
+        };
+        line[0].color = sf::Color::Green;
+        line[1].color = sf::Color::Yellow;
+        line[2].color = sf::Color::Red;
+        line[3].color = sf::Color::Blue;
+
+        window.draw(line, 4, sf::Lines);
 
         // Update the window
         window.display();
